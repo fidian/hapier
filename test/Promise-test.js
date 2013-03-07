807bbd21a3304da4ca5af06a7f30215efe18c737
Tyler Akins <fidian@rumkin.com>
2013-03-06 20:52:16 -0600

    Promises are now up to spec

--- test/Promise-test.js
+++ test/Promise-test.js
@@ -3,10 +3,12 @@
 
 'use strict';
 
-var tests, Promise;
+var tests, Promise, vows, assert;
 
 tests = require('promises-aplus-tests');
 Promise = require('../lib/Promise');
+vows = require('vows');
+assert = require('assert');
 
 var adapter = {
 	pending: function () {
@@ -23,6 +25,17 @@ var adapter = {
 	}
 };
 
-tests(adapter, function (err) {
-	console.log(err);
+exports.batch = vows.describe('../lib/Promise.js').addBatch({
+	'a+': {
+		topic: function () {
+			var cb = this.callback;
+			tests(adapter, function (err) {
+				cb(err);
+			});
+		},
+
+		'they all pass': function (err) {
+			assert.ifError(err);
+		}
+	}
 });
